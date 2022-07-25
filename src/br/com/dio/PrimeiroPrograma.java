3b366b0035654d7b4d471777405b02d09b95f709
Débora Mendes de Azevedo <debora.map@hotmail.com>
2022-07-25 17:02:54 -0300

    Criando Class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -12,6 +12,43 @@ public class PrimeiroPrograma {
        Gato gato = new Gato();
        System.out.println(gato);
 
+
+        Livro livroTeste = new Livro("teste nome", 20);
+        System.out.println(livroTeste);
+
 }
 
+}
+class Livro{
+    private String nome;
+    private Integer numeroPaginas;
+
+    public Livro(String nome, Integer numeroPaginas) {
+        this.nome = nome;
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumeroPaginas() {
+        return numeroPaginas;
+    }
+
+    public void setNumeroPaginas(Integer numeroPaginas) {
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numeroPaginas=" + numeroPaginas +
+                '}';
+    }
 }
